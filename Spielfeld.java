1d1e399deb60abb99fdaa3dc28eeb361c8ce6815
Clemens Zander <cle.zander@gmail.com>
2019-05-13 17:50:27 +0200

    Kommentare in der Klasse Spielfeld hinzugefügt

--- Spielfeld.java
+++ Spielfeld.java
@@ -3,10 +3,9 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 /**
- * Beschreiben Sie hier die Klasse Spielfeld.
- * 
- * @author (Projekt Hüpfburg(alle)) 
- * @version (10.5.19)
+ * Die Klasse Spielfeld erstellt das Fenster, in welchem das Spiel angezeigt wird
+ * Author(Cihan Karahan, Shium Rahman, Gideon Schafroth)
+ * Version(10.5.19)
  */
 public class Spielfeld
 {
@@ -15,7 +14,13 @@ public class Spielfeld
    private String titel;
    private int breite;
    private int hoehe;
-
+   
+   /**
+    * Konstruktor der Klasse Spielfeld
+    * @param title Titel der über dem Fenster angezeigt wird
+    * @param width die Breite des Fensters
+    * @param height die Höhe des Fensters
+    */
    public Spielfeld(String title, int width, int height)
    {
       titel = title;
@@ -29,7 +34,7 @@ public class Spielfeld
       frame.setLocationRelativeTo(null);
       frame.setVisible(true);
 
-      canvas = new Canvas();
+      canvas = new Canvas();//Das ganze Fenster ist ein Canvas, also eine Zeichenfläche um Grafiken dort anzeigen zu können
       canvas.setPreferredSize(new Dimension(breite, hoehe));
       canvas.setMaximumSize(new Dimension(breite, hoehe));
       canvas.setMinimumSize(new Dimension(breite, hoehe));
@@ -38,11 +43,17 @@ public class Spielfeld
       frame.pack();
    }
 
+   /**
+    * gibt Canvas zurück
+    */
    public Canvas getCanvas()
    {
       return canvas;
    }
 
+   /**
+    * gibt das JFrame zurück
+    */
    public JFrame getFrame()
    {
       return frame;
